b20419260a4741bf807c9f55a5cc984601709085
mahendrareddy2003 <150582693+mahendrareddy2003@users.noreply.github.com>
2025-11-10 11:09:26 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+--Creating the database "DataWarehouse"
+CREATE DATABASE DataWarehouse;
+GO
+USE DataWarehouse;
+GO
+-- Creating Schemas in the same data base
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
